f1efd035b3b404e8a0a8073492506edce5924245
mortvola <shields12345@msn.com>
2022-06-13 05:57:38 -0700

    Moved the controls the map component so that they are available whether logged in or not.

--- client/Map/Map.tsx
+++ client/Map/Map.tsx
@@ -3,6 +3,8 @@ import {
   GoogleMap, HeatmapLayer, Marker, Polyline, useJsApiLoader,
 } from '@react-google-maps/api';
 import Http from '@mortvola/http';
+import Controls from './Controls';
+import styles from './Map.module.css';
 
 const containerStyle = {
   width: '100%',
@@ -19,19 +21,27 @@ const libraries: ['visualization'] = ['visualization'];
 type PropsType = {
   apiKey: string,
   showLocation?: boolean,
-  heatmap?: google.maps.LatLng[],
 }
 
-const Map: React.FC<PropsType> = ({ apiKey, showLocation = false, heatmap = [] }) => {
+const Map: React.FC<PropsType> = ({ apiKey, showLocation = false }) => {
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: apiKey,
     libraries,
   });
 
+  type HeatmapListEntry = {
+    id: number,
+    date: string,
+    map: null | google.maps.LatLng[],
+  };
+
   const [, setMap] = React.useState<google.maps.Map | null>(null);
   const [location, setLocation] = React.useState<{ lat: number, lng: number } | null>(null);
   const [trail, setTrail] = React.useState<{ lat: number, lng: number }[][] | null>(null);
+  const [heatmapList, setHeatmaplist] = React.useState<HeatmapListEntry[]>([]);
+  const [heatmap, setHeatmap] = React.useState<google.maps.LatLng[]>([]);
+  const [heatmapIndex, setHeatmapIndex] = React.useState<number>(0);
 
   useEffect(() => {
     if (isLoaded) {
@@ -63,6 +73,51 @@ const Map: React.FC<PropsType> = ({ apiKey, showLocation = false, heatmap = [] }
     }
   }, [isLoaded, showLocation]);
 
+  React.useEffect(() => {
+    (async () => {
+      const response = await Http.get<{ id: number, date: string }[]>('/api/heatmap-list');
+
+      if (response.ok) {
+        const body = await response.body();
+
+        setHeatmaplist(body.map((m) => ({
+          id: m.id,
+          date: m.date,
+          map: null,
+        })));
+
+        setHeatmapIndex(body.length - 1);
+      }
+    })();
+  }, []);
+
+  React.useEffect(() => {
+    (async () => {
+      const hm = heatmapList[heatmapIndex].map;
+
+      if (hm === null) {
+        const response = await Http.get<[number, number][]>(`/api/heatmap/${heatmapList[heatmapIndex].id}`);
+
+        if (response.ok) {
+          const body = await response.json();
+
+          heatmapList[heatmapIndex].map = body.map((p) => (
+            new google.maps.LatLng(p[1], p[0])
+          ));
+
+          setHeatmap(heatmapList[heatmapIndex].map ?? []);
+        }
+      }
+      else {
+        setHeatmap(hm);
+      }
+    })();
+  }, [heatmapIndex, heatmapList]);
+
+  const handleChange = async (value: number) => {
+    setHeatmapIndex(value);
+  };
+
   const onLoad = React.useCallback((m: google.maps.Map) => {
     setMap(m);
   }, []);
@@ -73,42 +128,50 @@ const Map: React.FC<PropsType> = ({ apiKey, showLocation = false, heatmap = [] }
 
   if (isLoaded) {
     return (
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={5}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-        options={{ minZoom: 5, streetViewControl: false }}
-      >
-        {
-          location
-            ? (
-              <Marker
-                position={location}
-                visible
-              />
-            )
-            : null
-        }
-        {
-          trail
-            ? (
-              trail.map((t, index) => (
-                <Polyline
-                  key={index}
-                  path={t}
-                  options={{ strokeWeight: 1 }}
+      <div className={styles.layout}>
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={5}
+          onLoad={onLoad}
+          onUnmount={onUnmount}
+          options={{ minZoom: 5, streetViewControl: false }}
+        >
+          {
+            location
+              ? (
+                <Marker
+                  position={location}
+                  visible
                 />
-              ))
-            )
-            : null
-        }
-        <HeatmapLayer
-          data={heatmap}
-          options={{ dissipating: true, opacity: 1 }}
+              )
+              : null
+          }
+          {
+            trail
+              ? (
+                trail.map((t, index) => (
+                  <Polyline
+                    key={index}
+                    path={t}
+                    options={{ strokeWeight: 1 }}
+                  />
+                ))
+              )
+              : null
+          }
+          <HeatmapLayer
+            data={heatmap}
+            options={{ dissipating: true, opacity: 1 }}
+          />
+        </GoogleMap>
+        <Controls
+          min={0}
+          max={heatmapList.length - 1}
+          onChange={handleChange}
+          value={heatmapIndex}
         />
-      </GoogleMap>
+      </div>
     );
   }
 
--- client/Welcome/Welcome.tsx
+++ client/Welcome/Welcome.tsx
@@ -1,48 +1,19 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import CookieConsent from 'react-cookie-consent';
-import Http from '@mortvola/http';
-import GoogleMap from '../Map/Map';
+import Map from '../Map/Map';
 import styles from './Welcome.module.css';
 import Login from './login/Login';
 import Register from './login/Register';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Controls from '../Map/Controls';
 
 type PropsType = {
   mapApiKey: string,
 }
 
 const App: React.FC<PropsType> = ({ mapApiKey }) => {
-  type HeatmapListEntry = {
-    id: number,
-    date: string,
-    map: null | google.maps.LatLng[],
-  };
-
   const [showLogin, setShowLogin] = React.useState(false);
   const [showRegister, setShowRegister] = React.useState(false);
-  const [heatmapList, setHeatmaplist] = React.useState<HeatmapListEntry[]>([]);
-  const [heatmap, setHeatmap] = React.useState<google.maps.LatLng[]>([]);
-  const [heatmapIndex, setHeatmapIndex] = React.useState<number>(0);
-
-  React.useEffect(() => {
-    (async () => {
-      const response = await Http.get<{ id: number, date: string }[]>('/api/heatmap-list');
-
-      if (response.ok) {
-        const body = await response.body();
-
-        setHeatmaplist(body.map((m) => ({
-          id: m.id,
-          date: m.date,
-          map: null,
-        })));
-
-        setHeatmapIndex(body.length - 1);
-      }
-    })();
-  }, []);
 
   const handleLoginClick = () => {
     setShowLogin(true);
@@ -60,46 +31,13 @@ const App: React.FC<PropsType> = ({ mapApiKey }) => {
     setShowRegister(false);
   };
 
-  const handleChange = async (value: number) => {
-    setHeatmapIndex(value);
-  };
-
-  React.useEffect(() => {
-    (async () => {
-      const hm = heatmapList[heatmapIndex].map;
-
-      if (hm === null) {
-        const response = await Http.get<[number, number][]>(`/api/heatmap/${heatmapList[heatmapIndex].id}`);
-
-        if (response.ok) {
-          const body = await response.json();
-
-          heatmapList[heatmapIndex].map = body.map((p) => (
-            new google.maps.LatLng(p[1], p[0])
-          ));
-
-          setHeatmap(heatmapList[heatmapIndex].map ?? []);
-        }
-      }
-      else {
-        setHeatmap(hm);
-      }
-    })();
-  }, [heatmapIndex, heatmapList]);
-
   return (
     <div className={styles.layout}>
       <div className={styles.toolbar}>
         <button type="button" onClick={handleRegisterClick}>Register</button>
         <button type="button" onClick={handleLoginClick}>Login</button>
       </div>
-      <GoogleMap apiKey={mapApiKey} heatmap={heatmap} />
-      <Controls
-        min={0}
-        max={heatmapList.length - 1}
-        onChange={handleChange}
-        value={heatmapIndex}
-      />
+      <Map apiKey={mapApiKey} />
       <CookieConsent>
         This site uses cookies to enhance the user experience.
       </CookieConsent>
