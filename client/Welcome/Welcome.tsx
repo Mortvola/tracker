047797acd1bd0322970f9c68cf4cc4a4762efbe0
mortvola <shields12345@msn.com>
2022-06-14 18:59:25 -0700

    Removed the Register button.

--- client/Welcome/Welcome.tsx
+++ client/Welcome/Welcome.tsx
@@ -4,7 +4,6 @@ import CookieConsent from 'react-cookie-consent';
 import Map from '../Map/Map';
 import styles from './Welcome.module.css';
 import Login from './login/Login';
-import Register from './login/Register';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 type PropsType = {
@@ -13,7 +12,6 @@ type PropsType = {
 
 const App: React.FC<PropsType> = ({ mapApiKey }) => {
   const [showLogin, setShowLogin] = React.useState(false);
-  const [showRegister, setShowRegister] = React.useState(false);
 
   const handleLoginClick = () => {
     setShowLogin(true);
@@ -23,18 +21,9 @@ const App: React.FC<PropsType> = ({ mapApiKey }) => {
     setShowLogin(false);
   };
 
-  const handleRegisterClick = () => {
-    setShowRegister(true);
-  };
-
-  const handleRegisterHide = () => {
-    setShowRegister(false);
-  };
-
   return (
     <div className={styles.layout}>
       <div className={styles.toolbar}>
-        <button type="button" onClick={handleRegisterClick}>Register</button>
         <button type="button" onClick={handleLoginClick}>Sign In</button>
       </div>
       <Map apiKey={mapApiKey} />
@@ -42,7 +31,6 @@ const App: React.FC<PropsType> = ({ mapApiKey }) => {
         This site uses cookies to enhance the user experience.
       </CookieConsent>
       <Login show={showLogin} onHide={handleLoginHide} />
-      <Register show={showRegister} onHide={handleRegisterHide} />
     </div>
   );
 };
